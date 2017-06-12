b52ba3b5f289a76941f2325b8bfc1e884cb6edbf
Vargentum <vargentum@gmail.com>
2017-06-12 11:25:03 +0300

    add watch.clj to describe watching process

--- webapp/out/webapp/core.js
+++ webapp/out/webapp/core.js
@@ -2,6 +2,6 @@
 goog.provide('webapp.core');
 goog.require('cljs.core');
 cljs.core.enable_console_print_BANG_.call(null);
-cljs.core.println.call(null,"Hello world!");
+cljs.core.println.call(null,"Hello world me!");
 
 //# sourceMappingURL=core.js.map
